a6392eb95083a9f045cf3735f38a4c5a8a8c5c79
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2021-11-26 18:28:49 -0500

    Update segtree.cpp

--- alds/segtree.cpp
+++ alds/segtree.cpp
@@ -3,32 +3,45 @@ using namespace std;
 
 /**
  * Segment Tree supporting queries and updates in O(log(N))
- * f is any associate combiner function
+ * f is any associative combiner function
  * ZERO is any initial value such that f(ZERO, b) = b
  */
 
+template<class T>
 struct SegTree {
-    const long long ZERO = 0;
-    long long f(long long a, long long b) {
+    const T ZERO = 0;
+    T f(T a, T b) {
         return a + b;
     }
     int n;
-    vector<long long> segtree;
+    vector<T> segtree;
     void init(int _n) {
         n = _n;
         segtree.assign(2 * n, ZERO);
     }
+    void init(vector<T> v) {
+        init((int) v.size());
+        for (int i = 0; i < n; ++i) {
+            segtree[n + i] = v[i];
+        }
+        build();
+    }
+    void build() {
+        for (int i = n - 1; i > 0; --i) {
+            segtree[i] = f(segtree[i << 1], segtree[(i << 1) | 1]);
+        }
+    }
     void pull(int p) {
         segtree[p] = f(segtree[2 * p], segtree[2 * p + 1]);
     }
-    void update(int p, long long v) {
+    void update(int p, T v) {
         segtree[p += n] = v;
         for (p /= 2; p; p /= 2) {
             pull(p);
         }
     }
-    long long query(int a, int b) {
-        long long ra = ZERO, rb = ZERO;
+    T query(int a, int b) {
+        T ra = ZERO, rb = ZERO;
         for (a += n, b += n + 1; a < b; a /= 2, b /= 2) {
             if (a & 1) {
                 ra = f(ra, segtree[a++]);
