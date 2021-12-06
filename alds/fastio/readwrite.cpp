7f02c7ee57afadef59d716957eb6d9d9b7ee5923
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2021-12-06 18:55:16 -0500

    Update readwrite.cpp

--- alds/fastio/readwrite.cpp
+++ alds/fastio/readwrite.cpp
@@ -4,9 +4,13 @@ using namespace std;
 inline int getChar();
 inline int readChar();
 template <typename T = int> inline T readInt();
-template <typename T> inline void writeInt(T x, char last = 0);
+inline double readDouble();
+inline string readStr();
+
+template <typename T> inline void writeInt(T x, char c = 0);
+inline void writeDouble(double x, int d = 10, char c = 0);
 inline void writeChar(int x);
-inline void writeStr(const char *s);
+inline void writeStr(string s);
 
 static const int buffer_size = 4096;
 static int write_pos = 0;
@@ -48,6 +52,38 @@ inline T readInt() {
     return s == 1 ? x : -x;
 }
 
+inline double readDouble() {
+	int s = 1, c = readChar();
+	double x = 0;
+	if (c == '-') {
+		s = -1;
+		c = getChar();
+	}
+	while ('0' <= c && c <= '9') {
+		x = x * 10 + c - '0';
+		c = getChar();
+	}
+	if (c == '.') {
+		c = getChar();
+		double a = 1;
+		while ('0' <= c && c <= '9') {
+			x += (c - '0') * (a *= 1e-1);
+			c = getChar();
+		}
+	}
+	return s == 1 ? x : -x;
+}
+
+inline string readStr() {
+    int c = getChar();
+    string s = "";
+    while (c != -1 && c != '\n') {
+        s += c;
+        c = getChar();
+    }
+    return s;
+}
+
 inline void writeChar(int x) {
     if (write_pos == buffer_size) {
         fwrite(write_buffer, 1, buffer_size, stdout);
@@ -56,8 +92,8 @@ inline void writeChar(int x) {
     write_buffer[write_pos++] = x;
 }
 
-template <class T>
-inline void writeInt(T x, char last) {
+template <typename T>
+inline void writeInt(T x, char c) {
     if (x < 0) {
         writeChar('-');
         x = -x;
@@ -71,14 +107,38 @@ inline void writeInt(T x, char last) {
     while (n--) {
         writeChar(s[n]);
     }
-    if (last) {
-        writeChar(last);
+    if (c) {
+        writeChar(c);
     }
 }
 
-inline void writeStr(const char *s) {
-    while (*s) {
-        writeChar(*s++);
+inline void writeDouble(double x, int d, char c) {
+    if (x < 0) {
+        writeChar('-');
+        x = -x;
+    }
+    // assert(x <= (1LLU << 63) - 1);
+    long long t = (long long) x;
+    writeInt<long long>(t);
+    x -= t;
+    writeChar('.');
+    for (int i = d - 1; i > 0; --i) {
+        x *= 10;
+        t = min(9, (int) x);
+        writeChar('0' + t);
+        x -= t;
+    }
+    x *= 10;
+    t = min(9, (int) (x + 0.5));
+    writeChar('0' + t);
+    if (c) {
+        writeChar(c);
+    }
+}
+
+inline void writeStr(string s) {
+    for (const char &c : s) {
+        writeChar(c);
     }
 }
 
