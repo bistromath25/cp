61e6f70b06b32416a79cbc5a8e827167603c2d46
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2021-12-07 19:07:04 -0500

    Update readwrite.cpp
    endOfFile()

--- alds/fastio/readwrite.cpp
+++ alds/fastio/readwrite.cpp
@@ -1,40 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+inline bool endOfFile();
 inline int getChar();
+
 inline int readChar();
+inline string readStr();
 template <typename T = int> inline T readInt();
 inline double readDouble();
-inline string readStr();
 
-template <typename T> inline void writeInt(T x, char c = 0);
-inline void writeDouble(double x, int d = 10, char c = 0);
 inline void writeChar(int x);
 inline void writeStr(string s);
+template <typename T> inline void writeInt(T x, char c = 0);
+inline void writeDouble(double x, int d = 10, char c = 0);
 
 static const int buffer_size = 4096;
-static int write_pos = 0;
-static char write_buffer[buffer_size];
+static int read_pos = 0, read_length = 0, write_pos = 0;
+static char read_buffer[buffer_size], write_buffer[buffer_size];
+
+inline bool endOfFile() {
+    if (read_pos == read_length) {
+        read_pos = 0;
+        read_length = fread(read_buffer, 1, buffer_size, stdin);
+        if (read_pos == read_length) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 inline int getChar() {
-    static char buffer[buffer_size];
-    static int len = 0, pos = 0;
-    if (pos == len) {
-        pos = 0;
-        len = fread(buffer, 1, buffer_size, stdin);
-    }
-    if (pos == len) {
-        return -1;
-    }
-    return buffer[pos++];
+    return endOfFile() ? -1 : read_buffer[read_pos++];
 }
 
 inline int readChar() {
     int c = getChar();
-    while (c <= 32) {
+    while (c != - 1 && c <= 32) {
         c = getChar();
     }
     return c;
+}
+
+inline string readStr() {
+    int c = getChar();
+    string s = "";
+    while (c != -1 && c != '\n') {
+        s += c;
+        c = getChar();
+    }
+    return s;
 }
 
 template <typename T>
@@ -74,22 +88,18 @@ inline double readDouble() {
 	return s == 1 ? x : -x;
 }
 
-inline string readStr() {
-    int c = getChar();
-    string s = "";
-    while (c != -1 && c != '\n') {
-        s += c;
-        c = getChar();
-    }
-    return s;
-}
-
 inline void writeChar(int x) {
     if (write_pos == buffer_size) {
         fwrite(write_buffer, 1, buffer_size, stdout);
         write_pos = 0;
     }
     write_buffer[write_pos++] = x;
+}
+
+inline void writeStr(string s) {
+    for (const char &c : s) {
+        writeChar(c);
+    }
 }
 
 template <typename T>
@@ -136,12 +146,6 @@ inline void writeDouble(double x, int d, char c) {
     }
 }
 
-inline void writeStr(string s) {
-    for (const char &c : s) {
-        writeChar(c);
-    }
-}
-
 struct Flusher {
     ~Flusher() {
         if (write_pos) {
@@ -150,3 +154,5 @@ struct Flusher {
         }
     }
 } flusher;
+
+
