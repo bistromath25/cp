2cff7ad4b0a5088f00f1f04491561150a3b23f99
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2021-12-23 11:06:15 -0500

    Update bellman_ford.cpp

--- alds/graph/bellman_ford.cpp
+++ alds/graph/bellman_ford.cpp
@@ -11,9 +11,19 @@
         dist[i] = INF;  // some really big initial value
     }
     dist[x] = 0;
+    
     for (int i = 1; i <= n - 1; ++i) {  // run n - 1 times
         for (auto e : edges) {  // go through all the edges
             dist[e.b] = min(dist[e.b], dist[e.a] + e.w);  // attempt to reduce the distances
         }
     }
+     
+    for (auto e : edges) {  // check for negative cycle
+        if (dist[e.a] != LLINF && dist[e.a] + e.b < dist[e.b]) {  // if this path can be further improved
+            cout << "negative cycle\n";  // there must be a negative weight cycle
+            return;
+        }
+    }
+    
+    cout << "no negative cycle\n";
 }
